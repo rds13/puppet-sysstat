3b9380101e132090d14b5fb7dddfa5135f0318be
Mick Pollard <github@lunix.com.au>
2010-07-29 12:28:19 +1000

    add a documentation header to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,28 @@
+#vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+##############################################################
+# @filename : init.pp
+# @created : 12 Feb 2010 09:05:47 +1100
+# @last changed: Thur 29 Jul 2010 12:15:21 +1000
+# @author : Mick Pollard <aussielunix@gmail.com>
+##############################################################
+#
+# Class: sysstat
+#
+# This class installs sysstat, and takes care of setting up sar and sadc  
+# to gather disk/io stats as well as CPU.  
+# Only tested on CentOS/RHEL5.
+#
+# Parameters:
+#	$sardays	-	number of days to keep stats for.
+#				-	defaults to 28
+#
+# Actions:
+#   Ensures the sysstat package is installed and sets up the config files  
+#	based on the server achitecture
+#
+# Requires:
+#   - Package["sysstat"]
+#
 class sysstat {
 
   package { "sysstat" : ensure => present }
